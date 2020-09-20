a7c6cae4f8590a308194c12fbd3c6f81f9473be1
Chris Friedman <chris.s.friedman@gmail.com>
2020-09-20 12:54:33 -0400

    :fire: remove commented out placeholder code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,57 +1,26 @@
 const path = require("path")
 const slash = require("slash")
 var slugify = require("slugify")
-const axios = require('axios'); // to pull the fermentation api
-const crypto = require(`crypto`);
+const axios = require("axios") // to pull the fermentation api
+const crypto = require(`crypto`)
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-// exports.onCreateNode = ({
-//   node,
-//   actions,
-//   getNode
-// }) => {
-//   const {
-//     createNodeField
-//   } = actions
-//   if (node.internal.type === "beers__recipes" && node.id != "dummy") {
-//     const slug = slugify(node.title)
-//     console.log(slug)
-//     createNodeField({
-//       node,
-//       name: `slug`,
-//       value: slug,
-//     })
-//   }
-//   if (node.internal.type === "beers__brewsessions" && node.id != "dummy") {
-//     const slug = slugify("bs-" + node.recipe_title + "-bsno-" + node.batchcode)
-//     console.log(slug)
-//     createNodeField({
-//       node,
-//       name: `slug`,
-//       value: slug,
-//     })
-//   }
-// }
-
 let config = {
   headers: {
     "X-API-KEY": process.env.brewersfriend_api_key,
-  }
+  },
 }
 
-const fetchFermentation = (session_id) => (axios.get(`https://api.brewersfriend.com/v1/fermentation/` + session_id, config));
+const fetchFermentation = session_id =>
+  axios.get(
+    `https://api.brewersfriend.com/v1/fermentation/` + session_id,
+    config
+  )
 
-exports.onCreateNode = ({
-  node,
-  actions,
-  getNode
-}) => {
-  const {
-    createNode,
-    createNodeField
-  } = actions
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNode, createNodeField } = actions
   if (node.internal.type === "beers__recipes" && node.id != "dummy") {
     const slug = slugify(node.title)
     console.log(slug)
@@ -71,19 +40,29 @@ exports.onCreateNode = ({
     })
     let session_id = node.alternative_id
     console.log(session_id)
-    axios.get(`https://api.brewersfriend.com/v1/fermentation/` + session_id, config)
-      .then(function (res) {
-        console.log("|------------------|\n"+
-                    "|  fetched "+ session_id + "  |\n" +
-                    "| status code: " + res.status + " |\n" +
-                    "|------------------|")
+    axios
+      .get(
+        `https://api.brewersfriend.com/v1/fermentation/` + session_id,
+        config
+      )
+      .then(function(res) {
+        console.log(
+          "|------------------|\n" +
+            "|  fetched " +
+            session_id +
+            "  |\n" +
+            "| status code: " +
+            res.status +
+            " |\n" +
+            "|------------------|"
+        )
         if (res.status == 200) {
-          res.data.readings.map((reading) => {
+          res.data.readings.map(reading => {
             // Create your node object
             const readingNode = {
               // Required fields
               id: reading.id,
-              parent: '__SOURCE__',
+              parent: "__SOURCE__",
               internal: {
                 type: `fermentationReading`, // name of the graphQL query --> allRandomUser {}
                 // contentDigest will be added just after
@@ -108,20 +87,20 @@ exports.onCreateNode = ({
               ip: reading.ip,
               recipe_id: reading.recipe_id,
               brewevent_id: reading.brewevent_id,
-              session_id: session_id
+              session_id: session_id,
             }
 
             // Get content digest of node. (Required field)
             const contentDigest = crypto
               .createHash(`md5`)
               .update(JSON.stringify(readingNode))
-              .digest(`hex`);
+              .digest(`hex`)
             // add it to userNode
-            readingNode.internal.contentDigest = contentDigest;
+            readingNode.internal.contentDigest = contentDigest
 
             // Create node with the gatsby createNode() API
-            createNode(readingNode);
-          });
+            createNode(readingNode)
+          })
         }
       })
       .catch(function(error) {
@@ -129,100 +108,23 @@ exports.onCreateNode = ({
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
+          console.log(error.response.data)
+          console.log(error.response.status)
         } else if (error.request) {
           // The request was made but no response was received
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
           // http.ClientRequest in node.js
-          console.log(error.request);
+          console.log(error.request)
         } else {
           // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
+          console.log("Error", error.message)
         }
-        console.log(error.config);
-      });
+      })
   }
 }
 
-// // Query the fermentation api
-// exports.sourceNodes = async ({
-//   actions,
-// }) => {
-//   const {
-//     createNode
-//   } = actions;
-//
-//   let config = {
-//     headers: {
-//       "X-API-KEY": process.env.brewersfriend_api_key,
-//     }
-//   }
-//
-//   let session_id = '346187'
-//
-//   const fetchFermentation = (session_id) => (axios.get(`https://api.brewersfriend.com/v1/fermentation/` + session_id, config));
-//   // await for results
-//   const res = await fetchFermentation(session_id);
-//
-//   // map into these results and create nodes
-//   res.data.readings.map((reading) => {
-//     // Create your node object
-//     const readingNode = {
-//       // Required fields
-//       id: reading.id,
-//       parent: '__SOURCE__',
-//       internal: {
-//         type: `fermentationReading`, // name of the graphQL query --> allRandomUser {}
-//         // contentDigest will be added just after
-//         // but it is required
-//       },
-//       children: [],
-//
-//       // Other fields that you want to query with graphQl
-//       gravity: reading.gravity,
-//       gravity_unit: reading.gravity_unit,
-//       temp: reading.temp,
-//       temp_unit: reading.temp_unit,
-//       ph: reading.ph,
-//       comment: reading.comment,
-//       eventtype: reading.eventtype,
-//       created_at: reading.created_at,
-//       source: reading.source,
-//       name: reading.name,
-//       annotation: reading.annotation,
-//       interaval: reading.interval,
-//       beer: reading.beer,
-//       ip: reading.ip,
-//       recipe_id: reading.recipe_id,
-//       brewevent_id: reading.brewevent_id,
-//       session_id: session_id
-//     }
-//
-//     // Get content digest of node. (Required field)
-//     const contentDigest = crypto
-//       .createHash(`md5`)
-//       .update(JSON.stringify(readingNode))
-//       .digest(`hex`);
-//     // add it to userNode
-//     readingNode.internal.contentDigest = contentDigest;
-//
-//     // Create node with the gatsby createNode() API
-//     createNode(readingNode);
-//   });
-//
-//   return;
-// }
-
-
-exports.createPages = async ({
-  graphql,
-  actions
-}) => {
-  const {
-    createPage
-  } = actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
   const result = await graphql(`
     query {
       allBeersRecipes(
@@ -241,11 +143,9 @@ exports.createPages = async ({
       }
     }
   `)
-  result.data.allBeersRecipes.edges.forEach(({
-    node
-  }) => {
+  result.data.allBeersRecipes.edges.forEach(({ node }) => {
     createPage({
-      path: ("beers/" + node.alternative_id),
+      path: "beers/" + node.alternative_id,
       component: path.resolve("./src/pages/beer.js"),
       context: {
         slug: node.fields.slug,
